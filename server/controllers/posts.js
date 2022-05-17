4cbce2dd87cdc79ba786a63dfeac033e21bed785
meirson1 <noam220599@gmail.com>
2022-05-17 19:59:53 +0300

    Adding client side structure

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -10,8 +10,12 @@ export const getPosts = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
-  const body = req.body;
+  const post = req.body;
+  const newPost = new PostMessage(post);
   try {
-    res.send("Post Creation");
-  } catch (error) {}
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
