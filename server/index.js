fd7ebe3420367b1fb974cca05ccc8379ea90c912
Noam Meir <93981788+meirson1@users.noreply.github.com>
2022-06-22 23:00:47 +0300

    Need to fix DB

--- server/index.js
+++ server/index.js
@@ -7,14 +7,14 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: "true" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: "true" }));
 app.use(cors());
 
+app.use("/posts", postRoutes);
+
 const CONNECTION_URL =
-  "mongodb+srv://Noamson:noam220599@cluster0.kbsdq.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://Noamson:noam220599@cluster0.kbsdq.mongodb.net/SocialNetwork?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
